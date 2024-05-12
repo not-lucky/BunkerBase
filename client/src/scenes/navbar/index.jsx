3f6f8db5083061eca2d3f23676e774e756755e66
not-lucky <57046712+not-lucky@users.noreply.github.com>
2024-05-12 09:10:59 +0530

    login, register functionality;\nBUG: Can upload any file.

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -39,8 +39,8 @@ const Navbar = () => {
   const primaryLight = theme.palette.primary.light;
   const alt = theme.palette.background.alt;
 
-  // const fullName = `${user.firstName} ${user.lastName}`;
-  const fullName = "Ayuisajnd aiojsodj";
+  const fullName = `${user.firstName} ${user.lastName}`;
+  // const fullName = "Ayuisajnd aiojsodj";
 
   return (
     <FlexBetween padding="1rem 6%" backgroundColor={alt}>
