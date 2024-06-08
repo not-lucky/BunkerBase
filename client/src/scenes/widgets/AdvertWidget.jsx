3f0353eca729faa9a36276f57a399b105f52631e
not-lucky <57046712+not-lucky@users.noreply.github.com>
2024-06-08 06:17:11 +0530

    better advert

--- client/src/scenes/widgets/AdvertWidget.jsx
+++ client/src/scenes/widgets/AdvertWidget.jsx
@@ -24,12 +24,11 @@ const AdvertWidget = () => {
         style={{ borderRadius: "0.75rem", margin: "0.75rem 0" }}
       />
       <FlexBetween>
-        <Typography color={main}>MikaCosmetics</Typography>
-        <Typography color={medium}>mikacosmetics.com</Typography>
+        <Typography color={main}>An Average Advert</Typography>
+        <Typography color={medium}>something.com</Typography>
       </FlexBetween>
       <Typography color={medium} m="0.5rem 0">
-        Your pathway to stunning and immaculate beauty and made sure your skin
-        is exfoliating skin and shining like light.
+        Just an average advertisement. Use uBlock origin btw :)
       </Typography>
     </WidgetWrapper>
   );
